76519f0f12e1466878d5caae8ff79fa972a00570
HOUNSOU96 <deogratiashounsou@gmail.com>
2025-10-15 02:30:26 +0200

    Mise à jour configuration backend et corrections

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -52,7 +52,7 @@ const Login: React.FC = () => {
     setErreur(null);
 
     try {
-      const res = await fetch("http://localhost:8000/api/auth/login", {
+      const res = await fetch("https://code-backend-iuol.onrender.com/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
