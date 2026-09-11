e0f12a49edf9918fc67c185cf864d5d5a32197e6
HOUNSOU96 <deogratiashounsou@gmail.com>
2026-09-12 00:41:34 +0200

    Correction URL backend production

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,9 @@
-import axios from 'axios';
+
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000', // adapte selon ton backend
+  baseURL: "https://code-backend-iuol.onrender.com",
 });
 
 export default api;
+
